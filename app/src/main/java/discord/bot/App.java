51e2319ba6a06a53d3ac45b14ffbb02d414cb815
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-02-07 17:25:45 +0100

    Initial commit

---
+++ app/src/main/java/discord/bot/App.java
@@ -0,0 +1,49 @@
+package discord.bot;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.events.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+
+public class App implements EventListener
+{
+    public static void main(String[] args) throws LoginException, InterruptedException
+    {
+        String token;
+        try {
+            token = Files.readString(Paths.get("token"));
+        } catch (IOException e) {
+            token = "";
+        }
+
+        // Note: It is important to register your ReadyListener before building
+        JDA jda = JDABuilder.createDefault(token)
+            .addEventListeners(new App())
+            .build();
+
+        // optionally block until JDA is ready
+        jda.awaitReady();
+    }
+
+    @Override
+    public void onEvent(GenericEvent event)
+    {
+        if (event instanceof ReadyEvent)
+            System.out.println("API is ready!");
+
+        if (event instanceof MessageReceivedEvent msgEvent) {
+            String msg = msgEvent.getMessage().getContentDisplay();
+
+            if (msg.contains("java") && !msgEvent.getAuthor().isBot()) {
+                msgEvent.getChannel().sendMessage("kocham jave kocham jave kocham jave <:javaSocialCredits:901211051369054208>").queue();
+            }
+        }
+
+    }
+}
