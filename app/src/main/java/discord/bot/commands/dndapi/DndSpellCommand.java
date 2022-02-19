b0ffbc0887b82983fb69a3bf0e4d6806993b1d9e
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-02-19 23:33:50 +0100

    Added creating embed for spell info

--- app/src/main/java/discord/bot/commands/dndapi/DndSpellCommand.java
+++ app/src/main/java/discord/bot/commands/dndapi/DndSpellCommand.java
@@ -2,15 +2,17 @@ package discord.bot.commands.dndapi;
 
 
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import discord.bot.CommandPermissions;
 import discord.bot.CommandPermissions.Permission;
 import discord.bot.commands.BotCommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class DndSpellCommand extends BotCommand {
@@ -29,9 +31,12 @@ public class DndSpellCommand extends BotCommand {
             
             var jsonMapper = new ObjectMapper();
             try {
-                String testString = getCallResultAsString();
+                String testString = getCallResultAsString("acid-arrow");
                 SpellData testClassInstance = jsonMapper.readValue(testString, SpellData.class);
-                event.getChannel().sendMessage(testClassInstance.toString()).queue();
+                event.getChannel().sendMessageEmbeds(SpellEmbedGenerator.generateSpellEmbed(testClassInstance)).queue();
+                testString = getCallResultAsString("acid-splash");
+                testClassInstance = jsonMapper.readValue(testString, SpellData.class);
+                event.getChannel().sendMessageEmbeds(SpellEmbedGenerator.generateSpellEmbed(testClassInstance)).queue();
             } catch (Exception e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -39,8 +44,9 @@ public class DndSpellCommand extends BotCommand {
         }
     }
 
-    private String getCallResultAsString() throws Exception{
-        URL url = new URL(API_ENDPOINT + "spells/acid-arrow");
+    private String getCallResultAsString(String spellIndex) throws Exception{
+        // URL url = new URL(API_ENDPOINT + "spells/acid-arrow");
+        URL url = new URL(API_ENDPOINT + "spells/" + spellIndex);
         // HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         var result = new StringBuilder();
         InputStream stream = url.openStream();
@@ -51,4 +57,119 @@ public class DndSpellCommand extends BotCommand {
         }
         return result.toString();
     }
+}
+
+class SpellEmbedGenerator {
+    
+    public static MessageEmbed generateSpellEmbed(SpellData spellData) {
+        var builder = new EmbedBuilder();
+
+        builder.setTitle(spellData.name());
+        builder.setColor(0x00dd22);
+
+        setDescription(spellData, builder);
+        setHigherLevelsInfo(spellData, builder);
+        setRangeInfo(spellData, builder);
+        setComponentsInfo(spellData, builder);
+        setMaterialInfo(spellData, builder);
+        setRitualInfo(spellData, builder);
+        setDurationInfo(spellData, builder);
+        setConcentrationInfo(spellData, builder);
+        setCastingTimeInfo(spellData, builder);
+        setSpellLevelInfo(spellData, builder);
+        setClassesInfo(spellData, builder);
+
+        return builder.build();
+    }
+
+    private static void setDescription(SpellData spellData, EmbedBuilder builder) {
+        var descriptionsParagraphs = spellData.desc();
+        
+        var descBuilder = new StringBuilder();
+
+        for (String paragraph : descriptionsParagraphs) {
+            descBuilder.append(paragraph);
+            descBuilder.append("\n");
+        }
+
+        builder.setDescription(descBuilder.toString());
+    }
+
+    private static void setHigherLevelsInfo(SpellData spellData, EmbedBuilder builder) {
+        var higherLevelsParagraphs = spellData.higherLevel();
+
+        if (higherLevelsParagraphs == null) return;
+
+        var higherLevelsBuilder = new StringBuilder();
+
+        for (String paragraph : higherLevelsParagraphs) {
+            higherLevelsBuilder.append(paragraph);
+            higherLevelsBuilder.append("\n");
+        }
+
+        builder.addField("At higher levels", higherLevelsBuilder.toString(), false);
+    }
+
+    private static void setRangeInfo(SpellData spellData, EmbedBuilder builder) {
+        String range = spellData.range();
+
+        if (range == null) return;
+
+        builder.addField("Range", range, false);
+    }
+
+    private static void setComponentsInfo(SpellData spellData, EmbedBuilder builder) {
+        var components = spellData.components();
+
+        if (components == null) return;
+
+        builder.addField("Components", Arrays.stream(components).collect(Collectors.joining(", ")), false);
+    }
+
+    private static void setMaterialInfo(SpellData spellData, EmbedBuilder builder) {
+        String matrerial = spellData.material();
+
+        if (matrerial == null) return;
+
+        builder.addField("Material", matrerial, false);
+    }
+
+    private static void setRitualInfo(SpellData spellData, EmbedBuilder builder) {
+        builder.addField("Is Ritual", spellData.ritual() ? "Yes" : "No", false);
+    }
+
+    private static void setDurationInfo(SpellData spellData, EmbedBuilder builder) {
+        String duration = spellData.duration();
+
+        if (duration == null) return;
+
+        builder.addField("Duration", duration, false);
+    }
+
+    private static void setConcentrationInfo(SpellData spellData, EmbedBuilder builder) {
+        builder.addField("Needs Concentration", spellData.concentration() ? "Yes" : "No", false);
+    }
+
+    private static void setCastingTimeInfo(SpellData spellData, EmbedBuilder builder) {
+        String castTime = spellData.castingTime();
+
+        if (castTime == null) return;
+
+        builder.addField("Casting Time", castTime, false);
+    }
+
+    private static void setSpellLevelInfo(SpellData spellData, EmbedBuilder builder) {
+        int spellLevel = spellData.level();
+        String spellLevelStr = String.valueOf(spellLevel) + (spellLevel == 0 ? " (Cantrip)" : "");
+        builder.addField("Spell level", spellLevelStr, false);
+    }
+
+    private static void setClassesInfo(SpellData spellData, EmbedBuilder builder) {
+        APIReference[] classes = spellData.classes();
+        
+        if (classes == null) return;
+        
+        builder.addField("Classes", Arrays.stream(classes).map(APIReference::name).collect(Collectors.joining(", ")), false);
+    }
+
 }
--- app/src/main/java/discord/bot/commands/dndapi/SpellData.java
+++ app/src/main/java/discord/bot/commands/dndapi/SpellData.java
@@ -1,5 +1,6 @@
 package discord.bot.commands.dndapi;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -27,8 +28,8 @@ public record SpellData(
 
 record Damage(
     @JsonProperty("damage_type") DamageType dmgType,
-    @JsonProperty("damage_at_slot_level") DamageAtSlotLevel dmgAtSlotLevels,
-    @JsonProperty("damage_at_character_level") DamageAtCharacterLevel dmgAtCharacterLevels
+    @JsonProperty("damage_at_character_level") DamageAtLevel dmgAtCharacterLevel,
+    @JsonProperty("damage_at_slot_level") DamageAtLevel dmgAtSpellslotLevel
 ) {}
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -38,24 +39,7 @@ record DamageType(
 ) {}
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-record DamageAtSlotLevel(
-    @JsonProperty("1") String lvl1Dmg,
-    @JsonProperty("2") String lvl2Dmg,
-    @JsonProperty("3") String lvl3Dmg,
-    @JsonProperty("4") String lvl4Dmg,
-    @JsonProperty("5") String lvl5Dmg,
-    @JsonProperty("6") String lvl6Dmg,
-    @JsonProperty("7") String lvl7Dmg,
-    @JsonProperty("8") String lvl8Dmg,
-    @JsonProperty("9") String lvl9Dmg
-) {
-    public String damage() {
-        
-    }
-}
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-record DamageAtCharacterLevel(
+record DamageAtLevel(
     @JsonProperty("1") String lvl1Dmg,
     @JsonProperty("2") String lvl2Dmg,
     @JsonProperty("3") String lvl3Dmg,
@@ -76,7 +60,33 @@ record DamageAtCharacterLevel(
     @JsonProperty("18") String lvl18Dmg,
     @JsonProperty("19") String lvl19Dmg,
     @JsonProperty("20") String lvl20Dmg
-) {}
+) {
+    public String damage(int level) {
+        return switch(level) {
+            case 1 -> lvl1Dmg;
+            case 2 -> lvl2Dmg;
+            case 3 -> lvl3Dmg;
+            case 4 -> lvl4Dmg;
+            case 5 -> lvl5Dmg;
+            case 6 -> lvl6Dmg;
+            case 7 -> lvl7Dmg;
+            case 8 -> lvl8Dmg;
+            case 9 -> lvl9Dmg;
+            case 10 -> lvl10Dmg;
+            case 11 -> lvl11Dmg;
+            case 12 -> lvl12Dmg;
+            case 13 -> lvl13Dmg;
+            case 14 -> lvl14Dmg;
+            case 15 -> lvl15Dmg;
+            case 16 -> lvl16Dmg;
+            case 17 -> lvl17Dmg;
+            case 18 -> lvl18Dmg;
+            case 19 -> lvl19Dmg;
+            case 20 -> lvl20Dmg;
+            default -> null;
+        };
+    }
+}
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 record APIReference(
